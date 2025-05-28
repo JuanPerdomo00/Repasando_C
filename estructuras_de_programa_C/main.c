92b831414f7a3ac71d17ac454fb4cd13bf0c8310
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-05-28 13:52:02 -0500

    Add: Complete 0

---
+++ estructuras_de_programa_C/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h> // incluir lib Estandar E/S 
+
+
+
+int main() { // Esta es la funcion principal
+    printf("Hello, World\n");
+    printf("This is fine... :D\n");
+    printf("\t Tab ->\n");
+    return 0;   
+    /*
+     * by: Jakepys
+     * */
+}
+
+
+
